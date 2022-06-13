ffe0a2e5137d73b1e5c52f1645b88d2d229550e8
Jeromia <depuytjerome@gmail.com>
2022-06-13 15:33:52 +0900

    liste pokemon with class

--- pokemonapp/src/app/app.component.ts
+++ pokemonapp/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  template: `<h1>Welcome to you!</h1>`
 })
-export class AppComponent {
-  title = 'pokemonapp';
+export class AppComponent implements OnInit{
+  // states
+    pokemonList: Pokemon[] = POKEMONS;
+
+  // methods
+  ngOnInit(){
+    console.log(this.pokemonList);
+    this.selectPokemon(this.pokemonList[0]);
+  }
+  selectPokemon(pokemon: Pokemon){
+    console.log('Vous avez cliqué sur le pokemon', pokemon.name);
+  }
 }
