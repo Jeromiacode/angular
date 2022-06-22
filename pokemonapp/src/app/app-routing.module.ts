25bb439c0fb0d18ca8a8d9d0e0013314ce3d345e
Jeromia <depuytjerome@gmail.com>
2022-06-22 16:41:49 +0900

    login/logout

--- pokemonapp/src/app/app-routing.module.ts
+++ pokemonapp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
     redirectTo: 'pokemons',
     // évite les effets de bords
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '**',
